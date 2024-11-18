7058210b714fed34cb26bc37ee4da46a86f8c921
Kevin Lee <hikevinmlee@gmail.com>
2024-11-18 17:29:39 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# `every()` - Time based media for p5.js
+
+`every()` helps you schedule scene changes in p5.js and more, for easily working on time-based media.
+
+```javascript
+every(2).minutes
+  .show(draw1)
+  .show(draw2)
+  .show(draw3);
+```
+```javascript
+every(1).hours
+  .show(draw1)
+  .show(draw2);
+```
+
+The 24-hour clock piece by Christian Marclay: 
+```
+const videos = [vid1200, vid1201, ..., vid2359];
+every(1).seconds
+  .showAll(videos);
+```
+
+You can provide different times to different scenes; in this case the number in `every()` indicates the default time if no time is provided for a scene.
+
+```
+every(5).seconds
+  .show(draw1) // shows for 5 seconds
+  .show(draw2, 6)
+  .show(draw3, 4);
+```
+
+You can also only transition scenes if a condition is met; in this case, a scene ends when it has `reportDone()`'d, which optionally takes a delay.
+
+```javascript
+every(5).seconds
+  .show(draw1).until()
+  .show(draw2);
+function draw1(reportDone) {
+  if (mouseIsPressed) {
+    // end 2s after mouse press
+    reportDone(2000);
+  }
+}
+```
+
+A scene can be skipped if a certain condition is not met.
+
+```javascript
+every(5).seconds
+  .show(draw1)
+  .show(draw2).if(checkDraw2Visible);
+function checkDraw2Visible() {
+  return shouldShowDraw2;
+}
+```
+
+*Experimental* - You can also provide a timestamped list of events to `every()` to schedule events at specific times. Useful for playing back recorded performances i.e. MIDI performances.
+
+```javascript
+const recordedEvents = {0: [1], 5000: [2], 10000: [3]};
+every(recordedEvents).timestamp
+  .react(1, draw1)
+  .react(2, draw2)
+  .react(3, draw3);
+```
---
+++ thinking.js
@@ -0,0 +1,78 @@
+// Should it be cycle or do (or omit entirely)
+every(5).seconds.cycle([draw1, draw2, draw3]);
+every(5).seconds.do(draw1);
+// Every other - is this useful
+every(2).other().minutes.do(draw1);
+// Respond to events - is this part of the remit
+every().mousePress.do(draw1);
+every(4).other().mousePress(draw1);
+// .cycle doesn't handle different length scenes
+every(5).seconds.show(draw1, 5).show(draw2, 6).show(draw3, 5);
+// what if different scenes have different interactions?
+every(5)
+  .seconds.show(draw1, 5, scene1MousePress, scene1Keypress)
+  .show(draw2, 6)
+  .show(draw3, 5);
+// what if a scene should play until some condition is met?
+every(5)
+  .seconds.show(draw1, () => scene1Done)
+  .show(draw2)
+  .show(draw3);
+// or maybe scenes can return completions
+every(5).seconds.showUntilDone(draw1).show(draw2);
+draw1 = () => 'done';
+// What if a scene should just wait after a delay?
+every(5)
+  .seconds.showUntil(draw1, () => scene1Done)
+  .show(draw2);
+draw1 = () => {
+  setTimeout(() => (scene1Done = true), 5000);
+};
+// or promise style?
+every(5).seconds.showUntil(draw1).show(draw2);
+draw1 = (resolve) => {
+  setTimeout(resolve, 5000);
+};
+// or promise style with less presupposed knowledge on promises?
+every(5).seconds.showUntil(draw1).show(draw2);
+draw1 = (reportDone) => {
+  reportDone(5000);
+};
+// Should you be able to insert scenes?
+timeline = every(5).seconds.show(draw1).show(draw2);
+timeline.show(draw3);
+timeline.Nth(2).show(draw4); // inserts draw4 after draw2
+// Should scenes pass data to each other?
+every(5).seconds.showUntil(draw1).show(draw2);
+draw1 = (resolve) => {
+  resolve('foo');
+};
+draw2 = (resolve, lastSceneData) => {
+  console.log(lastSceneData);
+};
+// or maybe this should just be handled with global variables
+// Maybe, you'll insert a scene, and later want to turn it off? or maybe insteaad
+// of inserting/deleting you just need to toggle on/off?
+every(5).seconds.show(draw1).show(draw2).if(checkDraw2Visible);
+function checkDraw2Visible() {
+  return draw2Visible;
+}
+// What about running through a sequence of prerecorded events i.e. symphonyinacid.net
+every(midiEvent).timestamp.react(processMidiEvent);
+every(recordedEvents).timestamp.react(processRecordedEvent);
+const recordedEvents = {0: [1], 5000: [2], 10000: [3]};
+const processRecordedEvent = (event) => {
+  switch (event) {
+    case 1:
+      draw1();
+      break;
+    case 2:
+      draw2();
+      break;
+    case 3:
+      draw3();
+      break;
+  }
+};
+// or maybe avoiding the switch
+every(recordedEvents).timestamp.react(1, draw1).react(2, draw2).react(3, draw3);
