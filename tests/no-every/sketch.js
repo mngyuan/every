39748c0ef0e9546f652b2dd5b5266a7ee8117aed
Kevin Lee <hikevinmlee@gmail.com>
2024-11-27 14:17:55 +0000

    add tests

---
+++ tests/no-every/sketch.js
@@ -0,0 +1,9 @@
+// No errors should appear in console
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
---
+++ tests/push-pop/sketch.js
@@ -0,0 +1,21 @@
+// No errors should appear in console
+// Text should appear centered on the canvas and in two sizes
+
+function setup() {
+  createCanvas(400, 400);
+  textAlign(CENTER, CENTER);
+
+  every(1).seconds.show(draw1).show(draw2);
+}
+
+function draw() {
+  background(220);
+}
+
+function draw1() {
+  textSize(128);
+  text('Hello', 200, 200);
+}
+function draw2() {
+  text('I should be normal sized', 200, 200);
+}
