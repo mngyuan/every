2953b6a4fafc92a26a1efd0b4ca24db2d79339bc
Kevin Lee <hikevinmlee@gmail.com>
2024-11-27 14:14:19 +0000

    fix issue if every() isnt called

--- p5.every.js
+++ p5.every.js
@@ -101,83 +101,86 @@ p5.prototype.every = function (n) {
 };
 
 p5.prototype.chooseScene = function () {
-  if (!_context.timerPaused) {
-    let [i, len] = calculateCurrentScene(this.frameCount);
+  if (_context != undefined) {
+    if (!_context.timerPaused) {
+      let [i, len] = calculateCurrentScene(this.frameCount);
 
-    if (_context.scene != i) {
-      // Changing scenes
+      if (_context.scene != i) {
+        // Changing scenes
 
-      // Remove existing listeners
-      _context.mouseListeners.map((listener) =>
-        document.removeEventListener('mousedown', listener),
-      );
-      _context.keyboardListeners.map((listener) =>
-        document.removeEventListener('keydown', listener),
-      );
-      // Check new scene(s)'s predicate
-      let checked = [];
-      while (_context.sceneFs[i].if && _context.sceneFs[i].if()) {
-        if (checked.includes((i + 1) % _context.sceneFs.length)) {
-          // We've checked all scenes, none of them passed
-          break;
-        }
-        checked.push(i);
-        // skip to the start of the next scene
-        skipToNextScene(this.frameCount, len);
-        [i, len] = calculateCurrentScene(this.frameCount);
-      }
-      // Add new scene's listeners
-      if (_context.sceneFs[i].mousePressed) {
-        document.addEventListener(
-          'mousedown',
-          _context.sceneFs[i].mousePressed,
+        // Remove existing listeners
+        _context.mouseListeners.map((listener) =>
+          document.removeEventListener('mousedown', listener),
         );
-        _context.mouseListeners.push(_context.sceneFs[i].mousePressed);
-      }
-      if (_context.sceneFs[i].untilMousePressed) {
-        if (_context.sceneFs[i].passedLen) {
-          // Even though untilMousePressed was passed, a length was also passed, so
-          // this scene will still countdown
-        } else {
-          _context.timerPaused = true;
-        }
-        const listener = () => {
-          // just skip to the start of the next scene
+        _context.keyboardListeners.map((listener) =>
+          document.removeEventListener('keydown', listener),
+        );
+        // Check new scene(s)'s predicate
+        let checked = [];
+        while (_context.sceneFs[i].if && _context.sceneFs[i].if()) {
+          if (checked.includes((i + 1) % _context.sceneFs.length)) {
+            // We've checked all scenes, none of them passed
+            break;
+          }
+          checked.push(i);
+          // skip to the start of the next scene
           skipToNextScene(this.frameCount, len);
-        };
-        document.addEventListener('mousedown', listener);
-        _context.mouseListeners.push(listener);
-      }
-      if (_context.sceneFs[i].untilKeyPressed) {
-        if (_context.sceneFs[i].passedLen) {
-          // Even though untilKeyPressed was passed, a length was also passed, so
-          // this scene will still countdown
-        } else {
-          _context.timerPaused = true;
+          [i, len] = calculateCurrentScene(this.frameCount);
         }
-        const listener = (e) => {
-          // check keyCode, which is deprecated, but p5 still uses
-          if (
-            _context.sceneFs[_context.scene].untilKeyPressed === true ||
-            e.keyCode == _context.sceneFs[_context.scene].untilKeyPressed
-          ) {
+        // Add new scene's listeners
+        if (_context.sceneFs[i].mousePressed) {
+          document.addEventListener(
+            'mousedown',
+            _context.sceneFs[i].mousePressed,
+          );
+          _context.mouseListeners.push(_context.sceneFs[i].mousePressed);
+        }
+        if (_context.sceneFs[i].untilMousePressed) {
+          if (_context.sceneFs[i].passedLen) {
+            // Even though untilMousePressed was passed, a length was also passed, so
+            // this scene will still countdown
+          } else {
+            _context.timerPaused = true;
+          }
+          const listener = () => {
             // just skip to the start of the next scene
             skipToNextScene(this.frameCount, len);
+          };
+          document.addEventListener('mousedown', listener);
+          _context.mouseListeners.push(listener);
+        }
+        if (_context.sceneFs[i].untilKeyPressed) {
+          if (_context.sceneFs[i].passedLen) {
+            // Even though untilKeyPressed was passed, a length was also passed, so
+            // this scene will still countdown
+          } else {
+            _context.timerPaused = true;
           }
-        };
-        document.addEventListener('keydown', listener);
-        _context.keyboardListeners.push(listener);
-      }
+          const listener = (e) => {
+            // check keyCode, which is deprecated, but p5 still uses
+            if (
+              _context.sceneFs[_context.scene].untilKeyPressed === true ||
+              e.keyCode == _context.sceneFs[_context.scene].untilKeyPressed
+            ) {
+              // just skip to the start of the next scene
+              skipToNextScene(this.frameCount, len);
+            }
+          };
+          document.addEventListener('keydown', listener);
+          _context.keyboardListeners.push(listener);
+        }
 
-      if (_context.scene !== -1) {
-        pop();
+        if (_context.scene !== -1) {
+          pop();
+        }
+        push();
+        _context.scene = i;
       }
-      push();
-      _context.scene = i;
     }
-  }
 
-  _context.sceneFs[_context.scene] && _context.sceneFs[_context.scene].drawF();
+    _context.sceneFs[_context.scene] &&
+      _context.sceneFs[_context.scene].drawF();
+  }
 };
 p5.prototype.registerMethod('post', p5.prototype.chooseScene);
 p5.prototype.unregisterMethod('remove', p5.prototype.chooseScene);
